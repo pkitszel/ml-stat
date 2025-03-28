pkitszel/ml-stat

commit c58058fe939ba9c1b61afc005f935b073e05f560

ml-stat: add "pull-req" to PR patterns
Include "pull-req" as a pattern to look for when checking if given
email message is a PR. This is roughly half of the PR threads on netdev.

Signed-off-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>

ml-stat.py
@@ -83,6 +83,7 @@ class EmailPost:
 
     def is_pr(self):
         return 'pull req' in self._subject
+        return 'pull req' in self._subject or 'pull-req' in self._subject
 
     def is_bugzilla_forward(self):
         return 'Fw: [Bug ' in self._subject

commit c52f1ef13b1d9f24b10b8104515d4ed690ab3a81

ml-stat: remove stale "return" line
Commit cited via Fixes tag intended to add a second pattern to is_pr()
matching, but it left the previous logic intact. Fix it by removing
the stale return statement.
(I was adding to patch by one line, and somehow lost the removal).

Technically it is the moment that cited commit becomes effective.

Fixes: c58058fe939b ("ml-stat: add "pull-req" to PR patterns")
Signed-off-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>

ml-stat.py
@@ -82,7 +82,6 @@ class EmailPost:
         return self._subject[0] == '[' and not self.is_pr() and not self._is_discussion()
 
     def is_pr(self):
-        return 'pull req' in self._subject
         return 'pull req' in self._subject or 'pull-req' in self._subject
 
     def is_bugzilla_forward(self):